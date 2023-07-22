c05a5bed64c32fe0b177956fda11b2dfdabaf676
ashwinikumar96 <ashwinikumardash96@gmail.com>
2023-07-22 12:32:19 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def demo(request):
+    return render(request, 'demo.html')
+def index(request):
+    return render(request, 'index.html')
